6b779c2c933e521e8b5ea14d0b481144f19e3642
Alejandro_Almaraz <Alejandro_Almaraz@epam.com>
2022-08-21 18:12:17 +0200

    Include tables and schema for dbt trainning

--- main.tf
+++ main.tf
@@ -11,69 +11,143 @@ provider "snowflake" {
   role  = "SYSADMIN"
 }
 
-resource "snowflake_database" "db" {
-  name     = "TF_DEMO"
+
+resource "snowflake_database" "raw" {
+  name                        = "RAW"
+  comment                     = "Raw comment"
+  data_retention_time_in_days = 3
 }
 
-resource "snowflake_warehouse" "warehouse" {
-  name           = "TF_DEMO"
-  warehouse_size = "small"
-
-  auto_suspend = 60
+resource "snowflake_database" "analytics" {
+  name                        = "ANALYTICS"
+  comment                     = "Analytics comment"
+  data_retention_time_in_days = 3
 }
 
-provider "snowflake" {
-  alias = "security_admin"
-  role  = "SECURITYADMIN"
+resource "snowflake_schema" "jaffle_shop" {
+  database = snowflake_database.raw.name
+  name     = "JAFFLE_SHOP"
+  comment  = "A schema."
+
 }
 
-resource "snowflake_role" "role" {
-  provider = snowflake.security_admin
-  name     = "TF_DEMO_SVC_ROLE"
+resource "snowflake_schema" "stripe" {
+  database = snowflake_database.raw.name
+  name     = "STRIPE"
+  comment  = "A schema."
+
 }
 
-resource "snowflake_database_grant" "grant" {
-  provider          = snowflake.security_admin
-  database_name     = snowflake_database.db.name
-  privilege         = "USAGE"
-  roles             = [snowflake_role.role.name]
-  with_grant_option = false
+resource "snowflake_table" "table_customers" {
+  database            = snowflake_schema.jaffle_shop.database
+  schema              = snowflake_schema.jaffle_shop.name
+  name                = "CUSTOMERS"
+  comment             = "A table."
+
+  column {
+    name     = "ID"
+    type     = "integer"
+    nullable = true
+  }
+
+  column {
+    name     = "FIRST_NAME"
+    type     = "varchar"
+    nullable = true
+  }
+
+  column {
+    name = "LAST_NAME"
+    type = "varchar"
+  }
+
 }
 
-resource "snowflake_schema" "schema" {
-  database   = snowflake_database.db.name
-  name       = "TF_DEMO"
-  is_managed = false
+resource "snowflake_table" "table_orders" {
+  database            = snowflake_schema.jaffle_shop.database
+  schema              = snowflake_schema.jaffle_shop.name
+  name                = "ORDERS"
+  comment             = "A table."
+
+  column {
+    name     = "ID"
+    type     = "integer"
+    nullable = true
+  }
+
+  column {
+    name     = "USER_ID"
+    type     = "integer"
+    nullable = true
+  }
+
+  column {
+    name     = "ORDER_DATE"
+    type     = "date"
+    nullable = true
+  }
+
+  column {
+    name = "STATUS"
+    type = "varchar"
+  }
+
+  column {
+    name = "_ETL_LOADED_AT"
+    type = "timestamp"
+    default {
+      expression = "current_timestamp()"
+    }
+  }
+
 }
-resource "snowflake_schema_grant" "grant" {
-  provider          = snowflake.security_admin
-  database_name     = snowflake_database.db.name
-  schema_name       = snowflake_schema.schema.name
-  privilege         = "USAGE"
-  roles             = [snowflake_role.role.name]
-  with_grant_option = false
-}
-resource "snowflake_warehouse_grant" "grant" {
-  provider          = snowflake.security_admin
-  warehouse_name    = snowflake_warehouse.warehouse.name
-  privilege         = "USAGE"
-  roles             = [snowflake_role.role.name]
-  with_grant_option = false
-}
-resource "tls_private_key" "svc_key" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
-resource "snowflake_user" "user" {
-  provider          = snowflake.security_admin
-  name              = "tf_demo_user"
-  default_warehouse = snowflake_warehouse.warehouse.name
-  default_role      = snowflake_role.role.name
-  default_namespace = "${snowflake_database.db.name}.${snowflake_schema.schema.name}"
-  rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
-}
-resource "snowflake_role_grants" "grants" {
-  provider  = snowflake.security_admin
-  role_name = snowflake_role.role.name
-  users     = [snowflake_user.user.name]
+
+resource "snowflake_table" "payment" {
+  database            = snowflake_schema.stripe.database
+  schema              = snowflake_schema.stripe.name
+  name                = "PAYMENT"
+  comment             = "A table."
+
+  column {
+    name     = "ID"
+    type     = "integer"
+    nullable = true
+  }
+
+  column {
+    name     = "ORDERID"
+    type     = "integer"
+    nullable = true
+  }
+
+  column {
+    name = "PAYMENTMETHOD"
+    type = "varchar"
+  }
+
+  column {
+    name = "STATUS"
+    type = "varchar"
+  }
+
+
+  column {
+    name = "AMOUNT"
+    type = "integer"
+  }
+
+  column {
+    name = "CREATED"
+    type = "date"
+  }
+
+  column {
+    name = "_BATCHED_AT"
+    type = "timestamp"
+    default {
+      expression = "current_timestamp()"
+    }
+  }
+
+
 }
